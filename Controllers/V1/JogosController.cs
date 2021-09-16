72f188d64b48c165d106f99a2c0bc1d8f579feb3
Doug Oliveira <douggodie@gmail.com>
2021-09-16 00:25:09 -0300

    Commit Inicial

---
+++ Controllers/V1/JogosController.cs
@@ -0,0 +1,54 @@
+﻿using ApiCatalogoJogos.InputModel;
+using ApiCatalogoJogos.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Controllers.V1
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class JogosController : ControllerBase
+    {
+         [HttpGet]
+        public async Task<ActionResult<List<JogoViewModel>>> Obter()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{idJogo:guid}")]
+        public async Task<ActionResult<JogoViewModel>> Obter(Guid idJogo)
+        {
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<JogoViewModel>> InserirJogo(JogoInputModel jogo)
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo,JogoInputModel  jogo)
+        {
+            return Ok();
+        }
+        [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, double preco)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{idJogo:guid}")]
+        public async Task<ActionResult> ApagarJogo(Guid idJogo)
+        {
+            return Ok();
+        }
+
+
+    }
+}
